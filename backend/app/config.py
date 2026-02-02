def1fed23abb221e3dcd6860816925e2aa910fee
dayan iskhakov <dayan.iskhakov@gmail.com>
2026-02-02 10:47:11 +0500

    toker_ env must be int

--- backend/app/config.py
+++ backend/app/config.py
@@ -13,4 +13,4 @@ JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "dsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsds
 
 JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
 
-TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60)
+TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
